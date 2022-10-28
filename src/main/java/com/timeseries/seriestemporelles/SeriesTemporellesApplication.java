6c4ca08b4832abd26e62992d758ef101f71bee2d
HaKuDsi <hadikudsi@gmail.com>
2022-10-28 16:26:08 +0200

    first commit

---
+++ src/main/java/com/timeseries/seriestemporelles/SeriesTemporellesApplication.java
@@ -0,0 +1,13 @@
+package com.timeseries.seriestemporelles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesTemporellesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesTemporellesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timeseries/seriestemporelles/SeriesTemporellesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeseries.seriestemporelles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesTemporellesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
